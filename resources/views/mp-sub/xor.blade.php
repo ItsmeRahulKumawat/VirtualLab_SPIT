1117969a6068443eff83f464f200a85187d20b40
[Rahul Kumawat] <[rahulkumawat20000@gmail.com]>
2021-07-03 16:08:21 +0530

    Removed the java compiler from XOR page because its simulation is not in java

--- resources/views/mp-sub/xor.blade.php
+++ resources/views/mp-sub/xor.blade.php
@@ -74,16 +74,7 @@ body {font-size:16px;}
   <div class="w3-container" id="simulation" style="margin-top:75px">
     <h1 style="font-size:30px; margin-top:30px;"><b>Simulation.</b></h1>
     <hr style="width:50px;border:5px solid #A00202" class="w3-round">
-    <div data-pym-src=
-"https://www.jdoodle.com/iembed/v0/f37">
-    </div>
-    <script src=
-"https://www.jdoodle.com/assets/jdoodle-pym.min.js"
-        type="text/javascript">
-    </script>
   </div>
-  </div>
-
   <!-- Prolog experiment -->
   <div class="w3-container" id="prologExp" style="margin-top:75px">
     <h1 style="font-size:30px; margin-top:30px;"><b>Mp Neuron Problem Statements.</b></h1>
