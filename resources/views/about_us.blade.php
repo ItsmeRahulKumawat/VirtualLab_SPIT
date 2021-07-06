dac68a52d04745088e1d0ca17fb9fa9e176b3560
[Rahul Kumawat] <[rahulkumawat20000@gmail.com]>
2021-07-06 20:28:28 +0530

    About us page is ready with the final layout

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -1,34 +1,61 @@
-<link rel="stylesheet" href="/css/about_us.scss">
 @extends('template')
 @section('content')
 
-<div class="card-container">
-    <h1 class="heading"><span>meet</span>Our Team</h1>
+<link rel="stylesheet" href="/css/about_us.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link href="https://use.fontawesome.com/releases/v5.0.4/css/all.css" rel="stylesheet">
 
-    <div class="profiles">
-    <div class="profile">
-        <img src="img3.jpg" class="profile-img">
+<style>
+*{
+  font-family:verdana, sans-serif;
+}
+</style>
 
-        <h3 class="user-name">Dr.Aarti Karande</h3>
-        <h5>Project Manager</h5>
-        <p></p>
-      </div>
-      <div class="profile">
-        <img src="/images/about_us/rahul2.jpg" class="profile-img">
-
-        <h3 class="user-name">Rahul Kumawat</h3>
-        <h5>Developer</h5>
-        <p style="font-style:italic">You learn more from failure than from success.</p>
-      </div>
-      <div class="profile">
-        <img src="img2.jpg" class="profile-img">
-
-        <h3 class="user-name">Rutika Sonawane</h3>
-        <h5>Developer</h5>
-        <p></p>
-      </div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 col-sm-6">
+            <div class="our-team">
+                <div class="pic">
+                    <img src="/images/about_us/img3.jpg">
+                </div>
+                <h3 class="title">Dr.Aarti Karande</h3>
+                <span class="post">Project Manager</span>
+                <ul class="social">
+                <li><a href="" target="_blank" class="fab fa-github"></a></li>
+                    <li><a href="" target="_blank" class="fa fa-envelope"></a></li>
+                    <li><a href="" target="_blank" class="fab fa-linkedin"></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <div class="our-team">
+                <div class="pic">
+                    <img src="/images/about_us/rahul2.jpg">
+                </div>
+                <h3 class="title">Rahul Kumawat</h3>
+                <span class="post">Web Developer</span>
+                <ul class="social">
+                    <li><a href="https://github.com/ItsmeRahulKumawat" target="_blank" class="fab fa-github"></a></li>
+                    <li><a href="mailto: rahulkumawat20000.com" target="_blank" class="fa fa-envelope"></a></li>
+                    <li><a href="https://www.linkedin.com/in/rahul-kumawat-016b61201/" target="_blank" class="fab fa-linkedin"></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <div class="our-team">
+                <div class="pic">
+                    <img src="/images/about_us/img2.jpg">
+                </div>
+                <h3 class="title">Rutika Sonawane</h3>
+                <span class="post">Web Developer</span>
+                <ul class="social">
+                <li><a href="" target="_blank" class="fab fa-github"></a></li>
+                    <li><a href="" target="_blank" class="fa fa-envelope"></a></li>
+                    <li><a href="" target="_blank" class="fab fa-linkedin"></a></li>
+                </ul>
+            </div>
+        </div>
     </div>
-  </div>
-  <div class="footer_space"></div>
-    
-@stop   
+</div>
+<div class="footer_space"></div>
+@stop
