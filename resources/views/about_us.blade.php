90a50894108acfc757d8f6ef5bf58ae20b253e2c
[Rahul Kumawat] <[rahulkumawat20000@gmail.com]>
2021-07-06 15:10:07 +0530

    Added about us page some content with Rahul's image

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -1,5 +1,34 @@
-<link rel="stylesheet" href="/css/about_us.css">
+<link rel="stylesheet" href="/css/about_us.scss">
 @extends('template')
 @section('content')
 
+<div class="card-container">
+    <h1 class="heading"><span>meet</span>Our Team</h1>
+
+    <div class="profiles">
+    <div class="profile">
+        <img src="img3.jpg" class="profile-img">
+
+        <h3 class="user-name">Dr.Aarti Karande</h3>
+        <h5>Project Manager</h5>
+        <p></p>
+      </div>
+      <div class="profile">
+        <img src="/images/about_us/rahul2.jpg" class="profile-img">
+
+        <h3 class="user-name">Rahul Kumawat</h3>
+        <h5>Developer</h5>
+        <p style="font-style:italic">You learn more from failure than from success.</p>
+      </div>
+      <div class="profile">
+        <img src="img2.jpg" class="profile-img">
+
+        <h3 class="user-name">Rutika Sonawane</h3>
+        <h5>Developer</h5>
+        <p></p>
+      </div>
+    </div>
+  </div>
+  <div class="footer_space"></div>
+    
 @stop   
