7b40835515567692dc304ae64e08751b757a7f98
[Rahul Kumawat] <[rahulkumawat20000@gmail.com]>
2021-07-09 11:23:48 +0530

    Added images and bpn page

---
+++ resources/views/snn-sub/bpn.blade.php
@@ -0,0 +1,137 @@
+@extends('template')
+@section('content')
+<!DOCTYPE html>
+<html lang="en">
+   <title>BNN Neuron</title>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" src="/css/prolog.css">
+   <style>
+      body {font-size:16px;}
+      .w3-bar-item:hover {
+      text-decoration:none;
+      /* border-right:5px solid #CC0F0F; */
+      background-image: linear-gradient(to left,white,white,white,white,white,#CC0F0F);
+      border-radius:5px;
+      }
+      .w3-bar-item {
+      color:black;
+      }
+      .arrow-left{
+      margin-right:400px;
+      }
+      .arrow-right{
+      margin-left:400px;
+      }
+   </style>
+   <body>
+      <!-- Sidebar/menu -->
+      <nav class="w3-sidebar w3-red w3-collapse w3-top w3-large w3-padding" style="z-index:3;width:270px;font-weight:bold;" id="mySidebar">
+         <br>
+         <a href="javascript:void(0)" onclick="w3_close()" class="w3-button w3-hide-large w3-display-topleft" style="width:100%;font-size:22px">Close Menu</a>
+         <div class="w3-container">
+            <h3 class="w3-padding-64"></h3>
+         </div>
+         <div class="w3-bar-block" style="margin-top:-30px;">
+            <a style="color:black" onclick="w3_close(); confirmLeave('supervised_nn')" class="w3-bar-item w3-button w3-hover-white">Fuzzy Sets</a>
+            <a href="#intro" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Introduction</a> 
+            <a href="#procedure" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">Procedure</a> 
+            <a href="#simulation" onclick="w3_close()" class=" w3-bar-item w3-button w3-hover-white">Simulation</a> 
+            <!-- <a href="#prologExp" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white">CNN Problems</a>  -->
+            <a href="#feedback" onclick="w3_close()" class=" w3-bar-item w3-button w3-hover-white">Feedback</a>
+         </div>
+      </nav>
+      <!-- Top menu on small screens -->
+      <header class="w3-container w3-top w3-hide-large w3-red w3-xlarge w3-padding">
+         <a href="javascript:void(0)" class="w3-button w3-red w3-margin-right" onclick="w3_open()">&#9776;</a>
+         <span>options</span>
+      </header>
+      <!-- Overlay effect when opening sidebar on small screens -->
+      <div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+      <!-- !PAGE CONTENT! -->
+      <div class="w3-main" style="margin-left:250px;margin-right:50px">
+         <!-- Prolog -->
+         <div class="w3-container" style="margin-top:40px" id="intro">
+            <h1 class="w3-xxxlarge" style="color:#CC0F0F"><b>Supervised NN model using BPN</b></h1>
+            <h1 style="font-size:30px; margin-top:30px;"><b>Introduction.</b></h1>
+            <hr style="width:50px;border:5px solid #A00202" class="w3-round">
+            <b style="color:#cc0f0f">BPN (Back Propagation Neural Network)</b>
+                <p>Backpropagation is the essence of neural network training. It is the method of fine-tuning 
+                the weights of a neural network based on the error rate obtained in the previous epoch (i.e., iteration). 
+                Proper tuning of the weights allows you to reduce error rates and make the model reliable by increasing 
+                its generalization.</p>
+
+                <div class="space"></div>
+                    <img src="/images/snn-sub/bpn/snn.png" alt="image">
+                <div class="space"></div>
+                <p>The Backpropagation algorithm is a supervised learning method for multilayer feed-forward 
+                networks from the field of Artificial Neural Networks.</p>
+                <ul>
+                    <li>Backpropagation in neural network is a short form for "backward propagation of errors."</li>
+                    <li>It is a standard method of training artificial neural networks. </li>
+                    <li>This method helps calculate the gradient of a loss function with respect to all the weights in the network.</li>
+                </ul>
+            </div>
+            <!-- procedure -->
+            <div class="w3-container" id="procedure" style="margin-top:75px">
+               <h1 style="font-size:30px; margin-top:30px;"><b>Procedure.</b></h1>
+               <hr style="width:50px;border:5px solid #A00202" class="w3-round">
+               <p>The backpropagation algorithm is a method for training the weights in a multilayer feed-forward neural network. As such, it requires a network structure to be defined of one or more layers where one layer is fully connected to the next layer. A standard network structure is one input layer, one hidden layer, and one output layer.</p>
+                    <ul>
+                        <li>Initialize Network: Each neuron has a set of weights that need to be maintained.</li>
+                        <li>Forward Propagate: We can calculate an output from a neural network by propagating an input signal through each layer until the output layer outputs its values.</li>
+                        <li>Back Propagate Error: The errors are then propagated backward through the network from the output layer to the hidden layer, assigning blame for the error and updating weights as they go.</li>
+                        <li>Train Network: This involves first looping for a fixed number of epochs and within each epoch updating the network for each row in the training dataset.</li>
+                        <li>Predict: Making predictions with a trained neural network is easy enough.</li>
+                        <li>Seeds Dataset Case Study: First initializing a network, training it on the training dataset and then using the trained network to make predictions on a test dataset.</li>
+                    </ul>
+            </div>
+            <!-- simulation -->
+            <div class="w3-container" id="simulation" style="margin-top:75px">
+               <h1 style="font-size:30px; margin-top:30px;"><b>Simulation.</b></h1>
+               <hr style="width:50px;border:5px solid #A00202" class="w3-round">
+               <div data-pym-src="https://www.jdoodle.com/iembed/v0/f3s">
+               </div>
+               <script src="https://www.jdoodle.com/assets/jdoodle-pym.min.js"
+                  type="text/javascript"></script>
+            </div>
+         <!-- feedback -->
+         <div class="w3-container" id="feedback" style="margin-top:75px">
+            <h1 style="font-size:30px; margin-top:30px;"><b>Feedback.</b></h1>
+            <hr style="width:50px;border:5px solid #A00202" class="w3-round">
+            <p>Do you want us to improve something, Let us know we will try our best to improve it :) HAPPY LEARNING!</p>
+            <form action="/action_page.php" target="_blank">
+               <div class="w3-section">
+                  <label>Name</label>
+                  <input class="w3-input w3-border" type="text" name="Name" required>
+               </div>
+               <div class="w3-section">
+                  <label>Email</label>
+                  <input class="w3-input w3-border" type="text" name="Email" required>
+               </div>
+               <div class="w3-section">
+                  <label>Message</label>
+                  <input class="w3-input w3-border" type="text" name="Message" required>
+               </div>
+               <button type="submit" class="w3-button w3-block w3-padding-large w3-margin-bottom">Send Message</button>
+            </form>
+         </div>
+         <!-- End page content -->
+      </div>
+      <div class="footer_space"></div>
+      <script>
+         // Script to open and close sidebar
+         function w3_open() {
+           document.getElementById("mySidebar").style.display = "block";
+           document.getElementById("myOverlay").style.display = "block";
+         }
+          
+         function w3_close() {
+           document.getElementById("mySidebar").style.display = "none";
+           document.getElementById("myOverlay").style.display = "none";
+         }
+         
+      </script>
+   </body>
+</html>
+@stop
