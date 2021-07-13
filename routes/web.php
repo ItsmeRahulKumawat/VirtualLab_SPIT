4de52e9c3832c570489cec680820c41e2d63bf9c
[Rahul Kumawat] <[rahulkumawat20000@gmail.com]>
2021-07-13 19:33:46 +0530

    Added quiz pages with routes and tables made in database

--- routes/web.php
+++ routes/web.php
@@ -74,8 +74,15 @@ Route::view('/fuzzysets-sub/fuzzy','fuzzysets-sub/fuzzy')->name('fuzzyset-sub.fu
 //snn-sub
 Route::view('/snn-sub/bpn','snn-sub/bpn')->name('snn-sub.bpn');
 
+//assignment
+Route::view('/quiz/beginner','quiz/beginner')->name('quiz.beginner');
+Route::view('/quiz/intermediate','quiz/intermediate')->name('quiz.intermediate');
+Route::view('/quiz/advanced','quiz/advanced')->name('quiz.advanced');
+
 //fd form
 Route::post('/fd_subform',"fd_control@fd_subform_fn")->name('fd_subform');
+
+
 
 Auth::routes();
 
