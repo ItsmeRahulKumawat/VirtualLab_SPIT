285dc4a01fa5d26731c0ac9bf67d7e2d78eb6a91
[Rahul Kumawat] <[rahulkumawat20000@gmail.com]>
2021-07-13 20:50:25 +0530

    Code is fetching questions successfully from database with the page_id(but for right now hard coded)

--- resources/views/quiz/beginner.blade.php
+++ resources/views/quiz/beginner.blade.php
@@ -3,11 +3,31 @@
 @extends('template')
 @section('content')
 
+<style>
+
+</style>
+<?php
+
 $con = mysqli_connect('localhost','root');
-if($con){
-    echo " success";
-}
 mysqli_select_db($con, 'quiz_db')
+
+?>
+
+<div class="col-lg-8 m-auto d-block w3-margin-top">
+    <?php
+        $q = "select * from questions where page_id='prolog.easy'";
+        $query = mysqli_query($con, $q);
+
+        while($rows = mysqli_fetch_array($query) ) {
+    ?>
+        <div class="card">
+            <h4 class="card-header"> <?php echo $rows['questions'] ?> </h4>
+        </div>
+
+    <?php
+        }
+        ?>
+</div>
 
 
 
