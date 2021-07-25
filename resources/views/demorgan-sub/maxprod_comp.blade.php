2b7a7a393caa22be08dd13425ef6ea1e88639537
[Rahul Kumawat] <[rahulkumawat20000@gmail.com]>
2021-07-25 18:28:03 +0530

    Removed the feedback code and used extended code from another balde from all pages done

--- resources/views/demorgan-sub/maxprod_comp.blade.php
+++ resources/views/demorgan-sub/maxprod_comp.blade.php
@@ -207,53 +207,7 @@
                </div>
             </div>
          </div>
-         <!-- feedback -->
-  <div class="w3-container" id="feedback" style="margin-top:75px">
-    <h1 style="font-size:30px; margin-top:30px;"><b>Feedback.</b></h1>
-    <hr style="width:50px;border:5px solid #A00202" class="w3-round">
-    <p>Do you want us to improve something, Let us know we will try our best to improve it :) HAPPY LEARNING!</p>
-    <form action="{{url('/comment')}}" method="post" enctype="multipart/form-data">
-    @csrf
-      <div class="w3-section">
-         <input type="hidden" name="Page" value="demorgan">
-         <input type="hidden" name="SubPage" value="maxprod_comp">
-        <label>Name</label>
-        <input class="w3-input w3-border" type="text" name="Name" required>
-      </div>
-      <div class="w3-section">
-        <label>Email</label>
-        <input class="w3-input w3-border" type="email" name="Email" required>
-      </div>
-      <div class="w3-section">
-        <label>Message</label>
-        <input class="w3-input w3-border" type="text" name="comment" required>
-      </div>
-      <input type="submit" style="width:200px;" value="Send Message" class="w3-block btn btn-outline-secondary w3-padding-large w3-margin-bottom">
-    </form>       
-  </div>
-  <div class="w3-container" id="RecentFeedbacks" style="margin-top:75px">
-    <h1 style="font-size:30px; margin-top:30px;"><b>Recent Feedbacks.</b></h1>
-    <hr style="width:50px;border:5px solid #A00202" class="w3-round">
-    
-    
-        @forelse($comments as $comment)
-
-      <div class="card" id="feedbacklist">
-      <h2 class="card-header">{{$comment->Name}}</h2>
-      <div class="card-body">
-         <h5 class="card-title">{{$comment->comment}}.</h5>
-         
-         <button class="btn" id="green"><i class="fa fa-thumbs-up fa-lg" aria-hidden="true" style="color:blue"></i></button>
-      <button class="btn" id="red"><i class="fa fa-thumbs-down fa-lg" aria-hidden="true" style="color:red"></i></button>
-      </div>
-      </div>       
-      @empty
-         <h4>No Feedbacks</h4>
-      @endforelse
-
-
-</div>
-  </div>
+         @include('feedback-section.feedback')
          <!-- End page content -->
       </div>
       <div class="footer_space"></div>
